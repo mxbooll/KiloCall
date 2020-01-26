f63c0406520b4f9c1bf63f4acd1d4a4872c1a417
mxbooll <mxbooll@gmail.com>
2020-01-26 14:31:44 +0500

    Create User model

---
+++ KiloCall.Core/Model/User.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace KiloCall.Core.Model
+{
+    /// <summary>
+    /// Пользователь.
+    /// </summary>
+    public class User
+    {
+        #region Свойства
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Пол.
+        /// </summary>
+        public Gender Gender { get; }
+
+        /// <summary>
+        /// Дата рожддения.
+        /// </summary>
+        public DateTime BirthDay { get; }
+
+        /// <summary>
+        /// Вес.
+        /// </summary>
+        public double Weight { get; set; }
+
+        /// <summary>
+        /// Рост.
+        /// </summary>
+        public double Height { get; set; } 
+        #endregion
+
+        /// <summary>
+        /// Создать нового пользователя.
+        /// </summary>
+        /// <param name="name"> Имя. </param>
+        /// <param name="gender"> Пол. </param>
+        /// <param name="birthday"> Дата рождения. </param>
+        /// <param name="weight"> Вес. </param>
+        /// <param name="height"> Рост. </param>
+        public User(string name, Gender gender, DateTime birthday, double weight, double height)
+        {
+            #region Проверка входных параметров
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым или null", nameof(name));
+            }
+
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Пол не может быть null.", nameof(gender));
+            }
+
+            if (birthday < DateTime.Parse("01.01.1900") || birthday >= DateTime.Now)
+            {
+                throw new ArgumentException("Невозможная дата рождения.", nameof(birthday));
+            }
+
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Вес не может быть меньше или равен нулю.", nameof(weight));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentException("Рост не может быть меньше или равен нулю.", nameof(height));
+            } 
+            #endregion           
+
+            Name = name;
+            Gender = gender;
+            BirthDay = birthday;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ KiloCall.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KiloCall.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
